3c0dea9b18b58019e9592d961e38a57397f02da5
michael <michael@valkyrie.circlelinego.com>
2024-10-10 16:02:11 -0600

    Progress Save

--- public/javascripts/namegen.js
+++ public/javascripts/namegen.js
@@ -1,34 +1,103 @@
+/*
+ *
+ *  SETUP
+ *
+ */
+
 partcount=2;
-//const container = document.querySelector("#container");
 const container = document.getElementById("container");
-
-//const btnSubPart = document.querySelector("#btnSubPart");
+const btnAddPart = document.getElementById("btnAddPart");
 const btnSubPart = document.getElementById("btnSubPart");
-btnSubPart.addEventListener("click", () => 
-{
-	subPartLast();
-});
-
-const btnAddPart = document.querySelector("#btnAddPart");
-btnAddPart.addEventListener("click", () => 
-{
-	addPart();
-});
-
 const partArray = new Array();
-for(Z=0;Z<partcount;Z++)
+var lists = [];
+
+async function main()
 {
-	addPart()
+	// BUILD LISTS 
+	var listsBack=getLists().then((result) => {
+		result.forEach(function (X)
+		{
+			lists.push(X);
+		})
+	});
+	const listsDiv = document.getElementById("liststatus");
+	listsDiv.innerHTML = "Lists Loaded"+lists;
+
+	// REMOVE/ADD BUTTONS
+
+	btnSubPart.addEventListener("click", () => 
+	{
+		subPartLast();
+	});
+
+	btnAddPart.addEventListener("click", () => 
+	{
+		addPart();
+	});
+
+
+	for(Z=0;Z<partcount;Z++)
+	{
+		addPart()
+	}
+
+
+	await sleep(0.1)
+	updateParts()
+
 }
+
+main()
+
+function sleep(sec) {
+	let ms=sec*1000
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+/*
+ *
+ *  LOAD LIST NAMES
+ *
+ */
+
+async function getLists()
+{
+	try
+	{
+		const res = await fetch('http://10.100.100.100:8080/partlist');
+		if(!res.ok)
+		{
+			throw new Error('Failed to fetch lists');
+		}
+		let lists = await res.json();
+		return lists;
+	}
+	catch(err)
+	{
+		console.log(err.message);
+	}
+}
+
+/*
+ *
+ *  ADD PART
+ *
+ */
 
 function addPart()
 {
 	let newPart = {};
 	newPart.position = partArray.length;
-	newPart.id = Math.floor(Math.random()*(8999)+1000);
+	newPart.text = Math.floor(Math.random()*(8999)+1000); // TEMP Function
 	partArray.push(newPart);
 	updateParts();
 }
+
+/*
+ *
+ *  REMOVE PART
+ *
+ */
 
 function subPartLast()
 {
@@ -51,40 +120,158 @@ function subPart(partIndex)
 	updateParts();
 }
 
+/*
+ *
+ *  UPDATE PART
+ *
+ */
+
 function updateParts()
 {
 	while (container.firstChild) 
 	{
 		container.removeChild(container.lastChild);
 	}
-	for(const partThis of partArray)
+	for(var partThis of partArray)
 	{
 		buildDiv(partThis)
 	}
-	jsonSTRING = JSON.stringify(partArray);
 }
+
+
+function generatePart(partType,partOptions)
+{
+	//Expected Values
+}
+
+/*
+ *
+ *  MOVE PART
+ *
+ */
+
+function comparePartPos(a,b)
+{
+	if(a.position < b.position)
+	{
+		return -1;
+	}
+	else if(a.position > b.position)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+function movePart(partFrom,partTo)
+{
+	let positionTemp = partFrom.position;
+	partFrom.position = partTo.position;
+	partTo.position = positionTemp;
+	partArray.sort(comparePartPos);
+	updateParts();
+}
+
+/*
+ *
+ *  BUILD DIVS
+ *
+ */
 
 function buildDiv(partThis)
 {
-	var partDiv = document.createElement('div');
-	partDiv = document.createElement("div");
-	partDiv.setAttribute("id","name"+partThis.position);
-	partDiv.classList.add("name");
-	//partDiv.textContent = generatePart(Type);
-	partDiv.textContent = "Test "+partThis.id+" "+partThis.position+'.....';
-	var partBtn = document.createElement('button');
-	partBtn.innerHTML = 'Remove';
-	partBtn.data=partThis.position;
-	partBtn.addEventListener("click", () => {
-		subPart(partThis.position);
+	// CREATE DIV
+	let partDiv = document.createElement("div");
+	partDiv.classList.add("part");
+
+	// DIV ID
+	let posDiv = document.createElement("div");
+	posDiv.classList.add("pos");
+	posDiv.textContent = partThis.position;
+	partDiv.appendChild(posDiv);
+
+	// DIV PART TEXT
+	let partTextDiv = document.createElement("div");
+	partTextDiv.classList.add("text");
+	partTextDiv.textContent = partThis.text;
+	partDiv.appendChild(partTextDiv);
+
+	// LISTS DROPDOWN
+
+	let listsDiv = document.createElement("div");
+	listsDiv.classList.add("selectdiv");
+	let listsSelector = document.createElement("select");
+	lists.forEach(function (listitem)
+	{
+		listsSelector.appendChild(new Option(listitem.name,listitem.path));
 	});
-	partDiv.appendChild(partBtn);
+	listsDiv.appendChild(listsSelector);
+	partDiv.appendChild(listsDiv);
+
+	// BUTTON UP
+
+	let partBtnUPDiv = document.createElement("div");
+	partBtnUPDiv.classList.add("buttondiv");
+	if(partThis.position > 0)
+	{
+		partBtnUPDiv.classList.add("buttondiv");
+		let partBtnUP = document.createElement('button');
+		partBtnUP.innerHTML = 'U';
+		partBtnUP.classList.add("button");
+		partBtnUP.data=partThis.position;
+		partBtnUP.addEventListener("click", () => 
+		{
+			movePart(partThis,partArray[partThis.position-1]);
+		});
+		partBtnUPDiv.appendChild(partBtnUP);
+	}
+	else
+	{
+		partBtnUPDiv.classList.add("dummy");
+	}
+	partDiv.appendChild(partBtnUPDiv);
+
+	// BUTTON DOWN
+	
+	let partBtnDNDiv = document.createElement("div");;
+	if(partThis.position < partArray.length-1)
+	{
+		partBtnDNDiv.classList.add("buttondiv");
+		let partBtnDN = document.createElement('button');
+		partBtnDN.innerHTML = 'D';
+		partBtnDN.classList.add("button");
+		partBtnDN.data=partThis.position;
+		partBtnDN.addEventListener("click", () => 
+		{
+			movePart(partThis,partArray[partThis.position+1]);
+		});
+		partBtnDNDiv.appendChild(partBtnDN);
+	}
+	else
+	{
+		partBtnDNDiv.classList.add("dummy");
+	}
+	partDiv.appendChild(partBtnDNDiv);
+
+	//DIV BUTTON REMOVE
+
+	if(partArray.length > 1)
+	{
+		let partBtnRMDiv = document.createElement("div");;
+		partBtnRMDiv.classList.add("buttondiv");
+		let partBtnRM = document.createElement('button');
+		partBtnRM.innerHTML = 'X';
+		partBtnRM.data=partThis.position;
+		partBtnRM.classList.add("button");
+		partBtnRM.addEventListener("click", () => {
+			subPart(partThis.position);
+		});
+		partBtnRMDiv.appendChild(partBtnRM);
+		partDiv.appendChild(partBtnRMDiv);
+	}
+
 	container.appendChild(partDiv);
 }
-
-function pullFromList(listName)
-{
-	let testVar = "updateParts";
-	window[testVar]();
-}
-updateParts();
